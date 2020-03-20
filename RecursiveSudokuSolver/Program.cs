717b3bc5aedbe7dab57b423e3b7a52ba331c856d
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2020-03-20 22:37:39 +0000

    Add project files.

---
+++ RecursiveSudokuSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecursiveSudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
